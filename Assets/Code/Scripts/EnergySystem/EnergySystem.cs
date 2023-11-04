d35bb71a1c88c1cc2cae1de91afcaaeba6957f19
Danshicu <Promchuk.danik@gmail.com>
2023-11-04 17:58:22 +0300

    Added basic functionality

---
+++ Assets/Code/Scripts/EnergySystem/EnergySystem.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Code.EnergySystem
+{
+
+
+    public class EnergySystem : MonoBehaviour
+    {
+        [Header("Development stage adds")] 
+        [SerializeField] private EnergySpender spenderPrefab;        
+        
+        
+        [SerializeField] private float energyRechargeRate;
+        [SerializeField] private int minimalEnergyToAddSpender;
+        [SerializeField] private int maxEnergy;
+        
+        public int MaxEnergy
+        {
+            get => maxEnergy;
+            set => maxEnergy = value > 0 ? value : 0;
+        }
+        public float CurrentEnergy
+        {
+            get => _currentEnergy;
+            set
+            {
+                _currentEnergy = Mathf.Clamp(value, 0, maxEnergy);
+                OnEnergyChanged?.Invoke(_currentEnergy);
+            }
+        }
+        public Action<float> OnEnergyChanged;
+        
+        private float _currentEnergy;
+        private List<EnergySpender> _spenders = new List<EnergySpender>();
+
+        
+        public void AddSpender(EnergySpender spender)
+        {
+            if (_currentEnergy >= minimalEnergyToAddSpender)
+            {
+                if (!_spenders.Contains(spender))
+                {
+                    _spenders.Add(spender);
+                }
+            }
+        }
+
+        public void RemoveSpender(EnergySpender spender)
+        {
+            if (_spenders.Contains(spender))
+            {
+                _spenders.Remove(spender);
+                spender.Unsubscribe();
+            }
+        }
+
+        private void RemoveAllSpenders()
+        {
+            foreach (var spender in _spenders)
+            {
+                spender.Unsubscribe();
+            }
+            
+            _spenders.Clear();
+        }
+
+        private void RecountEnergy()
+        {
+            if (_spenders.Count > 0)
+            {
+                foreach (var spender in _spenders)
+                {
+                    CurrentEnergy -= spender.EnergySpend;
+                }
+            }
+            else
+            {
+                CurrentEnergy += energyRechargeRate;
+            }
+        }
+
+        private void CheckCurrentEnergy()
+        {
+            if (_currentEnergy <= 0)
+            {
+                RemoveAllSpenders();
+            }
+        }
+        
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var newSpender = GameObject.Instantiate(spenderPrefab);
+                AddSpender(newSpender);
+            }
+            CheckCurrentEnergy();
+            RecountEnergy();
+        }
+
+        private void OnEnable()
+        {
+            CurrentEnergy = MaxEnergy;
+        }
+    }
+}
---
+++ Assets/Code/Scripts/EnergySystem/EnergyUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.EnergySystem
+{
+    public class EnergyUI : MonoBehaviour
+    {
+        [SerializeField] private EnergySystem energySystem;
+        [SerializeField] private Slider energySlider;
+
+        private void OnEnable()
+        {
+            energySystem.OnEnergyChanged += SetCurrentValue;
+        }
+
+        private void OnDisable()
+        {
+            energySystem.OnEnergyChanged -= SetCurrentValue;
+        }
+
+        private void Start()
+        {
+            SetMaxValue(energySystem.MaxEnergy);
+        }
+        
+        public void SetMaxValue(int value)
+        {
+            energySlider.maxValue = value;
+        }
+
+        public void SetCurrentValue(float value)
+        {
+            energySlider.value = value;
+        }
+        
+    }
+}
