30d38a7532849b8d759b976e16f013d6d67703a1
Shtrudell <>
2023-11-05 13:28:44 +0300

    shit button

--- Assets/Code/Scripts/Doors/SlidingDoor.cs
+++ Assets/Code/Scripts/Doors/SlidingDoor.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SlidingDoor : OpenableDoor
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform door;
+
+    [SerializeField] private Transform destination;
+
+    [SerializeField] private float speed;
+
+    private Coroutine open;
+
+    public override void OpenDoor()
     {
-        
+        Debug.Log("Open Door");
+        if(open == null)
+            open = StartCoroutine(OpenDoorCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator OpenDoorCoroutine()
     {
-        
+        float startTime = Time.time;
+        Vector3 startPosition = door.position;
+        while (Vector3.Distance(door.position, destination.position) > 0.1f)
+        {
+            float distanceCovered = (Time.time - startTime) * speed;
+            float journeyFraction = distanceCovered / Vector3.Distance(door.position, destination.position);
+
+            door.position = Vector3.Lerp(startPosition, destination.position, journeyFraction);
+            yield return null;
+        }
     }
 }
