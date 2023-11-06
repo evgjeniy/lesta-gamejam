6dc10d380f5c31456021a1109d7d588221f87749
Shtrudell <ilya.naumenko.work@gmail.com>
2023-11-06 14:28:34 +0300

    fade

--- Assets/Code/Scripts/SceneSwitcher.cs
+++ Assets/Code/Scripts/SceneSwitcher.cs
@@ -6,19 +6,28 @@ using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
     [SerializeField] string nextlevel;
+    [SerializeField] Animator transition;
 
     public void LoadNextLevel()
     {
-        SceneManager.LoadScene(nextlevel);
+        StartCoroutine(LoadScene(nextlevel));
     }
 
     public void Restart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        transition?.SetTrigger("Start");
+        yield  return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(sceneName);
     }
 }
